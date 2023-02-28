bd40e0b8ab129b32d6a26eed73dacc9247c14620
bvrbryn445 <beaverbryanantipolo@gmail.com>
2023-02-27 22:55:37 -0500

    refactor: separate code in modules

---
+++ app.js
@@ -0,0 +1,24 @@
+const { MONGODB_URI } = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect(MONGODB_URI)
+	.then(() => {
+		logger.info('connected to MongoDB')
+	})
+	.catch((error) => {
+		logger.error('error connecting to MongoDB:', error.message)
+	})
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
