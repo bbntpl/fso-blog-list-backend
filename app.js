4f326522aa87667669d36bec0a5d22a43ab61fde
bvrbryn445 <beaverbryanantipolo@gmail.com>
2023-03-04 19:49:51 -0500

    exercise: 4.20

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const config = require('./utils/config')
 const {
 	unknownEndpoint,
 	errorHandler,
-	requestLogger
+	requestLogger,
+	tokenExtractor
+
 } = require('./utils/middleware')
 const logger = require('./utils/logger')
 
@@ -29,6 +31,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
+app.use(tokenExtractor)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
--- utils/middleware.js
+++ utils/middleware.js
@@ -8,6 +8,21 @@ const requestLogger = (request, response, next) => {
 	next()
 }
 
+const tokenExtractor = (req, res, next) => {
+	// code that extracts the token
+	const authorization = req.get('authorization')
+	if (authorization && authorization.startsWith('Bearer ')) {
+		logger.info(
+			'Token:  ',
+			authorization.replace('Bearer ', '')
+		)
+	} else {
+		logger.info('Token:  ', '')
+	}
+
+	next()
+}
+
 const unknownEndpoint = (req, res) => {
 	res.status(404).send('Your page is not found')
 }
@@ -37,6 +52,7 @@ const errorHandler = (err, req, res, next) => {
 
 module.exports = {
 	requestLogger,
+	tokenExtractor,
 	unknownEndpoint,
-	errorHandler
+	errorHandler,
 }
